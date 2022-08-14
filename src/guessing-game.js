4f3324bdc23f1eb0138a79a93ff11f1c4e012093
Diana <diana.klimsmm@gmailcom>
2022-08-14 12:02:28 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.average = null;
+    }
 
-    setRange(min, max) {
+    setRange(min, max) { //этот метод принимает минимальное и максимальное значение диапазона чисел для угадывания
+        this.min = min;
+        this.max = max;
 
     }
 
-    guess() {
-
+    guess() { //этот метод возвращает кандидата на решение (вы делаете предположение, основанное на диапазоне и предыдущих предположениях)
+        this.average = Math.round((this.min + this.max)/2);
+        return this.average;
     }
 
-    lower() {
-
+    lower() { // этот метод вызывается, если предыдущий вызов guess() вернул число, которое больше, чем ответ
+        this.max = this.average;
     }
 
-    greater() {
-
+    greater() { //этот метод вызывается, если предыдущий вызов guess() вернул число, которое меньше, чем ответ
+        this.min = this.average;
     }
 }
 
 module.exports = GuessingGame;
+
+
+// function setRange(min, max) {
+// return [min, max]
+// }
+
+// console.log(setRange(100, 300))
